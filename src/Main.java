b8ca6a4bf1a6e90bb576409423b954ba8458ed42
Pedro Camacho <Pedro.Camacho@gmail.com>
2019-03-20 16:57:09 +0100

    Root Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		Tabla miTabla = new Tabla ();
+		miTabla.setVisible(true);
+	}
+}
---
+++ src/Tabla.java
@@ -0,0 +1,74 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class Tabla extends JFrame {
+
+	private JPanel contentPane;
+	private JTable TableTlf;
+	private JTextField txtNombre;
+	private JTextField txtTlf;
+	private JButton btnAlta;
+	private JButton btnBaja;
+	private JButton btnModificar;
+
+
+	/**
+	 * Create the frame.
+	 */
+	public Tabla() {
+		setTitle("Agenda Telefónica");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(25, 22, 384, 121);
+		contentPane.add(scrollPane);
+		
+		TableTlf = new JTable();
+		scrollPane.setViewportView(TableTlf);
+		TableTlf.setModel(new DefaultTableModel(
+			new String[][] {
+				{"Javier Fernández", "123456789"},
+				{"Santiago López", "987654321"},
+			},
+			new String[] {
+				"Nombre", "Teléfono"
+			}
+		));
+		
+		txtNombre = new JTextField();
+		txtNombre.setBounds(25, 154, 179, 20);
+		contentPane.add(txtNombre);
+		txtNombre.setColumns(10);
+		
+		txtTlf = new JTextField();
+		txtTlf.setColumns(10);
+		txtTlf.setBounds(214, 154, 195, 20);
+		contentPane.add(txtTlf);
+		
+		btnAlta = new JButton("Alta");
+		btnAlta.setBounds(29, 200, 89, 23);
+		contentPane.add(btnAlta);
+		
+		btnBaja = new JButton("Baja");
+		btnBaja.setBounds(169, 200, 89, 23);
+		contentPane.add(btnBaja);
+		
+		btnModificar = new JButton("Modificar");
+		btnModificar.setBounds(320, 200, 89, 23);
+		contentPane.add(btnModificar);
+	}
+}
